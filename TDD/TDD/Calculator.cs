c631ce2f6dcee2d940e1e52f82500d17a184631f
Vsevolod <vsevolod.gordienko99@gmail.com>
2019-02-07 23:32:53 +0300

    Test driven development

---
+++ TDD/TDD/Calculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers is null)
+            {
+                throw new ArgumentNullException("Parameter is null");
+            }
+
+            if (numbers.Equals(string.Empty))
+            {
+                return 0;
+            }
+
+            int result = 0;
+            int currrentNumber = 0;
+            string defaultDelimiter = ",";
+            StringBuilder builder = new StringBuilder("negatives not allowed - ");
+            CheckDelimiter(ref numbers, ref defaultDelimiter);
+
+            string[] array = numbers.Split(new string[] { defaultDelimiter, "\n" }, StringSplitOptions.None);
+            foreach (string number in array)
+            {
+                if (number.Length > 4)
+                {
+                    Console.WriteLine(number[0]);
+                    if (number[0] == '-')
+                    {
+                        builder.Append(number);
+                    }
+
+                    continue;
+                }
+
+                currrentNumber = Int32.Parse(number);
+                if (currrentNumber < 0)
+                {
+                    builder.Append(number);
+                }
+                else
+                {
+                    if (currrentNumber <= 1000)
+                    {
+                        result += currrentNumber;
+                    }
+                }
+            }
+
+            if (builder.Length != 24)
+            {
+                throw new ArgumentException(builder.ToString());
+            }
+
+            return result; 
+        }
+
+        public static void CheckDelimiter(ref string numbers, ref string oldDelimiter)
+        {
+            if (numbers.Length < 2 || numbers[0] != '/')
+            {
+                return;
+            }
+
+            int lastBracketPlace = numbers.IndexOf(']');
+            int delimiterLength = 1;
+            if (numbers[2] != '[' || (lastBracketPlace == -1 && numbers[2] == '['))
+            {
+                oldDelimiter = numbers[2].ToString();
+            }
+            else
+            {
+                delimiterLength = lastBracketPlace - 1;
+                oldDelimiter = numbers.Substring(3, lastBracketPlace - 3);
+            }
+
+            numbers = numbers.Substring(delimiterLength + 3);
+        }
+    }
+}
---
+++ TDD/TDDTests/CalculatorTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using NUnit.Framework;
+using TDD;
+
+namespace TDDTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddNull_Throws()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<ArgumentNullException>(() => calculator.Add(null));
+        }
+
+        [Test]
+        public void AddEmpty_Zero()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add(string.Empty), 0);
+        }
+
+        [Test]
+        public void AddOne_One()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1"), 1);
+        }
+
+        [Test]
+        public void AddTwo_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1,2"), 3);
+        }
+
+        [Test]
+        public void AddFive_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1,2,3,4,5"), 15);
+        }
+
+        [Test]
+        public void AddSeven_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1,5,3,4,5,2,19"), 39);
+        }
+
+        [Test]
+        public void AddWithLines_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1\n2,3"), 6);
+        }
+
+        [Test]
+        public void AddWithMultipleLines_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1\n2,7\n2,3"), 15);
+        }
+
+        [Test]
+        public void AddWithSemicolon_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("//;\n1;2"), 3);
+        }
+
+        [Test]
+        public void AddWithNegatives_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<ArgumentException>(() => calculator.Add("-3,-1,2,3"), "negatives not allowed - -3 -1");
+        }
+
+        [Test]
+        public void AddWithVeryBigNegativeNumbers_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<ArgumentException>(() => calculator.Add("1,-19000000000000000000"), "negatives not allowed - -19000000000000000000");
+        }
+
+        [Test]
+        public void AddWithVeryBigNumbers_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1,5,2,900,19000000000000000000"), 908);
+        }
+
+        [Test]
+        public void AddWithNumberBiggerThan1000_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("1,5,2,2900"), 8);
+        } 
+
+        [Test]
+        public void AddWithLongDelimiter_Summ()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(calculator.Add("//[***]\n1***2***3"), 6);
+        }
+    }
+}
